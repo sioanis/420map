5fc707deaed75f5fdf081bd52555ce3e4101a74f
Sue Ann Ioanis <sue.ioanis@leaflink.com>
2020-04-04 11:34:11 -0400

    Copy

--- src/App.js
+++ src/App.js
@@ -20,8 +20,9 @@ function App() {
           <a href="mailto:sueann.ioanis@gmail.com?Subject=Cannabis%20Map%20Data">
             Email
           </a>{" "}
-          if you see a discrepancy or update it <a href="https://github.com/sioanis/420map">on github</a>. <i>Last Updated: (3/28/2020)</i> by{" "} @sioanis
-          
+          if you see a discrepancy or{" "}
+          <a href="https://github.com/sioanis/420map">update it</a>.{" "}
+          <i>Last Updated: (3/28/2020)</i> by @sioanis
         </p>
       </header>
       <div className="map">
